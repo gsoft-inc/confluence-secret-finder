14062f3c6c0ec676e2587ec78d63a4c794fb669b
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2020-07-13 12:07:23 -0400

    Updated blacklist.

--- confluence_secret_finder/secrets/plugins/password_pattern_plugin.py
+++ confluence_secret_finder/secrets/plugins/password_pattern_plugin.py
@@ -8,7 +8,7 @@ class PasswordPatternPlugin(BasePlugin):
                  "RFC[0-9]{3}", "ES201[56]", "Office365", "System32", "log4net",
                  "VS20[0-9]{2}", "word2vec", "Graphics2D", r"[CS]A\d{4}", "Base64",
                  "AES256CBC", "ISO27001", r"\d+GB", r"\d+IOPS", "python3",
-                 r"\d{2,4}(x\d{2,4})px", r"\d{2,4}x\d{2,4}", r"\d{1,2}mins", r"\d{1,2}min\d{1,2}s?", r"\d{1,2}days",
+                 r"\d{2,4}(x\d{2,4})px", r"\d{2,4}x\d{2,4}", r"\d{1,2}(mins?|hours?)", r"\d{1,2}min\d{1,2}s?", r"\d{1,2}(months?|days?)",
                  r"ConsoleApp\d", r"\d{1,4}(k|m)bps", r"\d{1,4}ms", "KB\d+"]
 
     def __init__(self):
